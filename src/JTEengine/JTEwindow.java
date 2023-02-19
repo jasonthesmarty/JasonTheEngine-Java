7bae7aac2fdd9bb067b994434f20888ddbbe7a87
jasonthesmarty <125754286+jasonthesmarty@users.noreply.github.com>
2023-02-18 22:14:28 -0500

    Added Background Color Change

--- src/JTEengine/JTEwindow.java
+++ src/JTEengine/JTEwindow.java
@@ -41,6 +41,7 @@ public class JTEwindow {
 
     public void startOpenGL() {
         GL.createCapabilities();
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
         GL11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     }
 
@@ -51,5 +52,70 @@ public class JTEwindow {
     public void terminate() {
         GLFW.glfwDestroyWindow(window);
         GLFW.glfwTerminate();
+    }
+
+    private float[] FloatToFloatColor(float red, float green, float blue, float alpha) {
+        float[] colors = {red, green, blue, alpha};
+
+        // Red
+        if (colors[0] > 255) {
+            throw new IllegalArgumentException("RGB value: Red too high ( red > 255 ).");
+        }
+        else if (colors[0] < 0) {
+            throw new IllegalArgumentException("RGB value: Red too high ( red < 0 ).");
+        }
+        else {
+            colors[0] = colors[0]/255;
+        }
+
+        // Green
+        if (colors[1] > 255) {
+            throw new IllegalArgumentException("RGB value: Green too high ( green > 255 ).");
+        }
+        else if (colors[1] < 0) {
+            throw new IllegalArgumentException("RGB value: Green too high ( green < 0 ).");
+        }
+        else {
+            colors[1] = colors[1]/255;
+        }
+
+        // Blue
+        if (colors[2] > 255) {
+            throw new IllegalArgumentException("RGB value: Blue too high ( blue > 255 ).");
+        }
+        else if (colors[2] < 0) {
+            throw new IllegalArgumentException("RGB value: Blue too high ( blue < 0 ).");
+        }
+        else {
+            colors[2] = colors[2]/255;
+        }
+
+        // Alpha
+        if (colors[3] > 255) {
+            throw new IllegalArgumentException("Alpha value: Alpha too high ( alpha > 255 ).");
+        }
+        else if (colors[3] < 0) {
+            throw new IllegalArgumentException("Alpha value: Alpha too high ( alpha < 0 ).");
+        }
+        else {
+            colors[3] = colors[3]/255;
+        }
+
+        return colors;
+    }
+
+    public void changeColor(float red, float green, float blue, float alpha) {
+        float[] colors = FloatToFloatColor(red, green, blue, alpha);
+        GL11.glClearColor(colors[0], colors[1], colors[2], colors[3]);
+    }
+
+    public void changeColor(float red, float green, float blue) {
+        float[] colors = FloatToFloatColor(red, green, blue, 255);
+        GL11.glClearColor(colors[0], colors[1], colors[2], colors[3]);
+    }
+
+    public void changeColor(float[] color) {
+        float[] colors = FloatToFloatColor(color[0], color[1], color[2], color[3]);
+        GL11.glClearColor(colors[0], colors[1], colors[2], colors[3]);
     }
 }
