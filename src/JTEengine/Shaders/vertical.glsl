93127d399e38862deff4a6f06d2459c9ffe6fdba
jasonthesmarty <125754286+jasonthesmarty@users.noreply.github.com>
2023-02-20 16:43:35 -0500

    Added position and color

--- src/JTEengine/Shaders/vertical.glsl
+++ src/JTEengine/Shaders/vertical.glsl
@@ -1,5 +1,11 @@
 #version 330
 
-void main() {
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec4 vertexColorIn;
 
+out vec4 vertexColorOut;
+
+void main() {
+    gl_Position = vec4(position, 1.0f);
+    vertexColorOut = vec4(vertexColorOut);
 }
