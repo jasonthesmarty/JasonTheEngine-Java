184ee01074bdcd2d17ff419508b3ff5eb8bb9e80
jasonthesmarty <125754286+jasonthesmarty@users.noreply.github.com>
2023-02-20 21:09:24 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,34 @@
-import JTEengine.Shaders.JTEshaders;
 import JTEengine.Standard.JTEstandard;
 import JTEengine.Window.JTEinput;
 import JTEengine.Window.JTEwindow;
 
 public class Main {
     public static void main(String[] args) {
-        JTEwindow window = new JTEwindow(800, 600, "Java Application");
+        JTEwindow window = new JTEwindow(1600, 900, "Java Application");
         window.create();
+        window.startOpenGL();
 
         JTEinput input = new JTEinput();
 
-        JTEshaders shaders = new JTEshaders();
+        JTEstandard std = new JTEstandard();
+
+        boolean print = true;
 
         while(!window.close()) {
-            window.startOpenGL();
-            shaders.activate();
+            window.clearColorGL();
 
-            window.changeColor(100, 0, 0, 255);
+            window.changeColor(25, 125, 250, 255);
+
+            if (print) {
+                print = false;
+            }
 
             if (input.keyDown(256)) {
                 break;
             }
 
-            shaders.deactivate();
             window.update();
         }
-        shaders.terminate();
         window.terminate();
     }
 }
