1261b8f5b35e50f556ce601cb63f4d630334fad6
jasonthesmarty <125754286+jasonthesmarty@users.noreply.github.com>
2023-02-22 17:51:45 -0500

    Added Polygon Class

---
+++ src/JTEengine/Graphics/JTEpolygon.java
@@ -0,0 +1,47 @@
+package JTEengine.Graphics;
+
+import org.lwjgl.opengl.*;
+
+@SuppressWarnings({"SpellCheckingInspection", "unused"})
+public class JTEpolygon {
+
+    private int VAO, VBO, IBO;
+    float[] vertices;
+    int[] indices;
+
+    public JTEpolygon(float[] vertices, int[] indices) {
+        this.vertices = vertices;
+        this.indices = indices;
+
+        VAO = GL30.glGenVertexArrays();
+        GL30.glBindVertexArray(VAO);
+
+        VBO = GL30.glGenBuffers();
+        GL30.glBindBuffer(GL15.GL_ARRAY_BUFFER, VBO);
+        GL30.glBufferData(GL15.GL_ARRAY_BUFFER, vertices, GL15.GL_STATIC_DRAW);
+
+        IBO = GL30.glGenBuffers();
+        GL30.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, IBO);
+        GL30.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indices, GL15.GL_STATIC_DRAW);
+        GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
+    }
+
+    public void render() {
+        GL30.glBindVertexArray(VAO);
+        GL30.glEnableVertexAttribArray(0);
+
+        GL30.glDrawElements(GL11.GL_TRIANGLES, this.indices.length, GL11.GL_UNSIGNED_INT, 0);
+    }
+
+    public void terminate() {
+        GL30.glBindVertexArray(0);
+        GL30.glDeleteVertexArrays(VAO);
+        GL30.glDisableVertexAttribArray(0);
+
+        GL30.glBindBuffer(GL30.GL_ARRAY_BUFFER, 0);
+        GL30.glDeleteBuffers(VBO);
+        GL30.glDeleteBuffers(IBO);
+    }
+
+
+}
