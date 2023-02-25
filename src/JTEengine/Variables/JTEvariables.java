75a2794b97bac17d1e4ca2c44849fe4192dc68c7
jasonthesmarty <125754286+jasonthesmarty@users.noreply.github.com>
2023-02-25 03:05:46 -0500

    Added Variables Class
    Has colors in RGBA format

---
+++ src/JTEengine/Variables/JTEvariables.java
@@ -0,0 +1,20 @@
+package JTEengine.Variables;
+
+@SuppressWarnings({"SpellCheckingInspection", "unused"})
+public class JTEvariables {
+
+    // Colors:
+
+    public static final int[] COLOR_RED_RGBA = {255, 0 , 0, 255};
+    public static final int[] COLOR_ORANGE_RGBA = {255, 165, 0, 255};
+    public static final int[] COLOR_YELLOW_RGBA = {255, 255, 0, 255};
+    public static final int[] COLOR_GREEN_RGBA = {0, 255, 0, 255};
+    public static final int[] COLOR_BLUE_RGBA = {0, 0, 255, 255};
+    public static final int[] COLOR_PURPLE_RGBA = {128, 0, 128, 255};
+    public static final int[] COLOR_PINK_RGBA = {255, 0, 127, 255};
+    public static final int[] COLOR_BROWN_RGBA = {150, 75, 0, 255};
+    public static final int[] COLOR_GRAY_RGBA = {128, 128, 128, 255};
+    public static final int[] COLOR_BLACK_RGBA = {0, 0, 0, 255};
+    public static final int[] COLOR_WHITE_RGBA = {255, 255, 255, 255};
+
+}
