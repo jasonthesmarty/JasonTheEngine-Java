a0406acf3f472477b0af6ca2b0129a9dc9810c28
jasonthesmarty <125754286+jasonthesmarty@users.noreply.github.com>
2023-03-14 01:04:04 -0400

    Added Version and PI

--- src/JTEengine/Variables/JTEvariables.java
+++ src/JTEengine/Variables/JTEvariables.java
@@ -5,6 +5,11 @@ public class JTEvariables {
 
     // Colors:
 
+    public static final String VERSION_M = "VERSION_5W23";
+    public static final String VERSION_D = "VERSION_5W23_0314a";
+
+    public static final double PI = 3.1415926535897932384d;
+
     public static final int[] COLOR_RED_RGBA = {255, 0, 0, 255};
     public static final int[] COLOR_ORANGE_RGBA = {255, 165, 0, 255};
     public static final int[] COLOR_YELLOW_RGBA = {255, 255, 0, 255};
@@ -16,5 +21,6 @@ public class JTEvariables {
     public static final int[] COLOR_GRAY_RGBA = {128, 128, 128, 255};
     public static final int[] COLOR_BLACK_RGBA = {0, 0, 0, 255};
     public static final int[] COLOR_WHITE_RGBA = {255, 255, 255, 255};
+
 
 }
